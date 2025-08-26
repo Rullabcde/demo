563e5a74a296481b6985f0dcadeec0083d5cf409
Rullabcde <choirulrasyid09@gmail.com>
2025-08-26 10:08:29 +0700

    fix redis

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,30 +1,41 @@
 import Redis from "ioredis";
 
-const globalForRedis = globalThis as unknown as {
-  redis: Redis | undefined;
-};
+declare global {
+  var redis: Redis | undefined;
+}
 
 function createRedisClient() {
-  if (process.env.NEXT_PHASE?.startsWith("build")) {
-    console.log("Skipping Redis connection during build phase");
-    return undefined;
+  if (process.env.NODE_ENV !== "production" && global.redis) {
+    return global.redis;
   }
 
-  return (
-    globalForRedis.redis ??
-    new Redis({
-      host: process.env.REDIS_HOST || "redis",
+  if (process.env.NEXT_PHASE?.startsWith("build") || !process.env.REDIS_HOST) {
+    console.log(
+      "Redis client not initialized (build phase or missing env variables)."
+    );
+    return null;
+  }
+
+  try {
+    const client = new Redis({
+      host: process.env.REDIS_HOST,
       port: process.env.REDIS_PORT ? Number(process.env.REDIS_PORT) : 6379,
       password: process.env.REDIS_PASSWORD || undefined,
       retryStrategy: (times) => Math.min(times * 50, 2000),
       enableReadyCheck: false,
       maxRetriesPerRequest: undefined,
-    })
-  );
+    });
+    console.log("Redis client successfully connected.");
+
+    if (process.env.NODE_ENV !== "production") {
+      global.redis = client;
+    }
+
+    return client;
+  } catch (error) {
+    console.error("Failed to connect to Redis:", error);
+    return null;
+  }
 }
 
 export const redis = createRedisClient();
-
-if (process.env.NODE_ENV !== "production" && redis) {
-  globalForRedis.redis = redis;
-}
